2734880a96165dab412e09ecea3d67d29a333b75
Brett Zamir <brettz9@yahoo.com>
2020-04-23 22:18:19 +0800

    - Editorconfig: Properly enforce more frequently used 4 sp. indent except in JSON - Linting: Check `strict` and `indent`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
+'use strict';
+
 module.exports = {
-  extends: ['eslint:recommended'],
-  env: {
-    node: true
-  }
+    extends: ['eslint:recommended'],
+    env: {
+        node: true
+    },
+    rules: {
+        strict: ['error'],
+        indent: ['error', 4]
+    }
 };
