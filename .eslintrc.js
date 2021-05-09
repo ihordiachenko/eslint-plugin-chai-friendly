86119887f7c1387ab351365282a5b3fe3a28e68d
Ihor Diachenko <ih.diachenko@gmail.com>
2021-05-10 01:16:15 +0300

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,9 @@ module.exports = {
     env: {
         node: true
     },
+    parserOptions: {
+        ecmaVersion: 2020
+    },
     rules: {
         strict: ['error'],
         indent: ['error', 4]
