bba470250c8f3f20643984145d5ad255fd06bbad
Ihor Diachenko <ih.diachenko@gmail.com>
2024-01-08 03:18:53 +0200

    Updated dev dependencies & added new tests from the upstream

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
-'use strict';
-
 module.exports = {
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+            babelrc: false,
+            configFile: false,
+        },
+    },
     extends: ['eslint:recommended'],
     env: {
         node: true
-    },
-    parserOptions: {
-        ecmaVersion: 2020
     },
     rules: {
         strict: ['error'],
