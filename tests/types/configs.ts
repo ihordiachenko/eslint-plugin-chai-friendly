23888cb3212382e08c4bf17cfac922a80297c2b0
Ihor Diachenko <ih.diachenko@gmail.com>
2026-03-17 02:22:50 +0200

    Fixed ts typing issue with recommended config #46

---
+++ tests/types/configs.ts
@@ -0,0 +1,7 @@
+import plugin = require('../../lib/index.js');
+
+plugin.configs.recommendedFlat;
+plugin.configs.recommended;
+
+plugin.configs.recommendedFlat.rules['chai-friendly/no-unused-expressions'];
+plugin.configs.recommended.plugins[0];
